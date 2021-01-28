d0c824043ffeecfd31b251cd0f3f64ba80651458
sugarvalley <cqr@o2.pl>
2021-01-28 23:58:16 +0100

    added timer, only play button is working (still not perfectly)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import './App.css';
 
 function App() {
     const [seconds, setSeconds] = useState(0);
+    const [start, setStart] = useState(false);
+    const [stop, setStop] = useState(false)
+    useEffect(() => {
+        if (start) {
+            console.log('im here');
+            window.setInterval(() => {
+                console.log('tik tock');
+                setSeconds(seconds => seconds+1);
+            }, 1000);
+        }
+    }, [start]);
+
   return (
     <div className="App">
         <div className="Outer-circle"></div><div className="Inner-circle">
-          <p className="Timer">0:00</p>
+          <p className="Timer">{seconds}</p>
       </div>
           <div className="Buttons">
-              <button className="Play-button">
+              <button className="Play-button" onClick={() => setStart(true)}>
                   <div className="Triangle"></div>
               </button>
-              <button className="Pause-button">
-                <div className="Sticks">
-                    <div className="Stick1"></div>
-                    <div className="Stick2"></div>
+              <button className="Pause-button" onClick={() => setStart(false)}>
+                <div className="Outer-square">
+                    <div className="Inner-square"></div>
                 </div>
               </button>
-              <button className="Stop-button">
-
+              <button className="Stop-button" onClick={() => setStop(true)}>
+                    <div className="Square"></div>
               </button>
           </div>
     </div>
